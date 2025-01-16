c10ab02fc484a8722191d89c72dfff2c99f19231
campi <campionr6@gmail.com>
2025-01-16 20:16:09 -0300

    Commit teste incial github - alteracao no texto

---
+++ src/com/Modulo_1/Exercicios/Matrizes1.java
@@ -0,0 +1,32 @@
+package com.Modulo_1.Exercicios;
+
+public class Matrizes1 {
+    public static void main(String[] args) {
+
+        // aqui a linha e coluna nao tem as mesmas dimensoes
+
+        int[][] matriz = new int[3][2]; // primeiro indice é linha, o segundo é coluna
+
+        matriz[0][0] = 1;
+        matriz[0][1] = 2;
+        //matriz[0][2] = 3;
+        matriz[1][0] = 4;
+        matriz[1][1] = 5;
+        //matriz[1][2] = 6;
+        matriz[2][0] = 7;
+        matriz[2][1] = 8;
+        //matriz[2][2] = 9;
+
+        System.out.println("Elemento [0] [0]" + matriz[0][0]);
+
+        for (int linha = 0; linha < matriz.length; linha++){
+
+            for (int coluna =0; coluna < matriz[linha].length ; coluna++)  //aqui o length tbem é da linha
+
+                System.out.println(matriz[linha][coluna]);
+        }
+
+
+    }
+
+}
---
+++ src/com/Modulo_1/Exercicios/vo/Produto.java
@@ -0,0 +1,8 @@
+package com.Modulo_1.Exercicios.vo;
+
+public class Produto {
+
+    public String descricao;
+    public int quantidade;
+    public double preco;
+}

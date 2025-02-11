f5b36656e3e27520afe5e40366a2384b6744f1af
campi <campionr6@gmail.com>
2025-02-11 08:32:14 -0300

    Polimorfismo

--- src/com/Modulo_2/Calc_ferias_Polimorfismo/Service/ServiçoDeCalculo.java
+++ src/com/Modulo_2/Calc_ferias_Polimorfismo/Service/ServiçoDeCalculo.java
@@ -1,5 +1,7 @@
 package com.Modulo_2.Calc_ferias_Polimorfismo.Service;
 
+// essa interface não esta fazendo nada.. só esta aqui pra mostrar herança nas interfaces
+
 public interface ServiçoDeCalculo {
 
     double calcularTaxa();

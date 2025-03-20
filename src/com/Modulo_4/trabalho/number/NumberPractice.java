34f2fa2cf2ecd2d3c888cce3b7248504928581dd
campi <campionr6@gmail.com>
2025-03-20 19:00:19 -0300

    Modulo_4_trabalho 20_03_25

--- src/com/Modulo_4/trabalho/number/NumberPractice.java
+++ src/com/Modulo_4/trabalho/number/NumberPractice.java
@@ -1,45 +1,58 @@
 package com.Modulo_4.trabalho.number;
 
 import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class NumberPractice {
 
     private static final Integer[] NUMBERS = {4, 1, 1, 8, 1, 7, 3, 2, 3, 4};
 
+
     public List<Integer> numbersLessThan5() {
         // Filtre todos os números que são menores que 5
-        Arrays.stream(NUMBERS)
-          .filter(number -> number <5)
-          .collect(Collectors.toList());
-
-        return null;
+        List<Integer> numbersLessFive = Arrays.stream(NUMBERS)
+                .filter(number -> number < 5)
+                .collect(Collectors.toList());
+       return numbersLessFive;
 
     }
 
     public Set<Integer> removeRepeatNumbers() {
         // Remova os números repetidos
-        Arrays.stream(NUMBERS);
-        return null;
+        Set<Integer> numbersHash = Arrays.stream(NUMBERS)
+                .collect(Collectors.toSet());
+        return numbersHash;
     }
 
     public Integer sumAllNumbers() {
         // Calcule a soma total dos números.
-        Arrays.stream(NUMBERS);
-        return null;
+        Integer sum = Arrays.stream(NUMBERS)
+                .reduce(0, (a, b) -> a + b);
+
+        return sum;
     }
 
     public Integer sumNumbersWithoutRepeats() {
         // Some todos os números, porém não utilize os repetidos
-        Arrays.stream(NUMBERS);
-        return null;
+        Integer somaSemRepetidos = Arrays.stream(NUMBERS)
+                .distinct()
+                .mapToInt(Integer::intValue)
+                .sum();
+
+        return somaSemRepetidos;
     }
 
     public Map<Integer, Long> countRepeatedNumber() {
         // EXTRA
         // Conte quantas vezes cada número aparece na lista. Exemplo de retorno: 4 -> 2, 1 -> 3
-        Arrays.stream(NUMBERS);
-        return null;
+        Map<Integer, Long> result = Arrays.stream(NUMBERS)
+                .collect(Collectors.groupingByConcurrent(
+                        Function.identity(),
+                        Collectors.counting()
+                ));
+
+        return result;
     }
 
 }

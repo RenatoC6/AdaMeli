a31de8529a1dd05b9376d36edc6273c2e65891fd
campi <campionr6@gmail.com>
2025-03-24 15:12:12 -0300

    trabalho de exercicios com comentarios = 24_03

--- src/com/Modulo_4/trabalho/number/NumberPractice.java
+++ src/com/Modulo_4/trabalho/number/NumberPractice.java
@@ -11,43 +11,59 @@ public class NumberPractice {
 
     public List<Integer> numbersLessThan5() {
         // Filtre todos os números que são menores que 5
-        List<Integer> numbersLessFive = Arrays.stream(NUMBERS)
-                .filter(number -> number < 5)
-                .collect(Collectors.toList());
-       return numbersLessFive;
+        List<Integer> numbersLessFive = Arrays.stream(NUMBERS) // cria um stream a partir do array NUMBERS.
+                .filter(number -> number < 5) //filtra os números que são menores que 5.
+                .collect(Collectors.toList()); //coleta os números filtrados em uma lista.
+        return numbersLessFive;
     }
 
     public Set<Integer> removeRepeatNumbers() {
         // Remova os números repetidos
-        Set<Integer> numbersDistinct = Arrays.stream(NUMBERS)
+        Set<Integer> numbersDistinct = Arrays.stream(NUMBERS) //cria um stream a partir do array NUMBERS.
+        //   coleta os números em um conjunto (Set),
+        //   que automaticamente remove os números repetidos.
                 .collect(Collectors.toSet());
         return numbersDistinct;
     }
 
     public Integer sumAllNumbers() {
         // Calcule a soma total dos números.
-        Integer sum = Arrays.stream(NUMBERS)
-                .reduce(0, (a, b) -> a + b);
+        Integer sum = Arrays.stream(NUMBERS) //cria um stream a partir do array NUMBERS.
+        //calcula a soma total dos números no stream.
+        // O primeiro argumento 0 é o valor inicial da soma,
+        // e a expressão lambda (a, b) -> a + b é a operação de soma que é
+        // aplicada a cada par de números no stream.
+                   .reduce(0, (a, b) -> a + b);
         return sum;
     }
 
     public Integer sumNumbersWithoutRepeats() {
         // Some todos os números, porém não utilize os repetidos
+
+        //Cria um stream a partir do array NUMBERS.
+        // Isso permite que os elementos do array sejam processados de forma sequencial.
         Integer somaSemRepetidos = Arrays.stream(NUMBERS)
+        //Remove os elementos duplicados do stream.
+        // Isso significa que apenas os números únicos serão considerados na soma.
                 .distinct()
+        //Converte os elementos do stream de Integer para int.
+        // Isso é necessário porque o método sum() trabalha com primitivos int.
                 .mapToInt(Integer::intValue)
-                .sum();
+                .sum(); // Calcula a soma de todos os elementos do stream.
+
         return somaSemRepetidos;
     }
 
     public Map<Integer, Long> countRepeatedNumber() {
         // EXTRA
         // Conte quantas vezes cada número aparece na lista. Exemplo de retorno: 4 -> 2, 1 -> 3
-        Map<Integer, Long> result = Arrays.stream(NUMBERS)
+        Map<Integer, Long> result = Arrays.stream(NUMBERS) //cria um stream a partir do array NUMBERS.
+        //agrupa os números pelo próprio valor (Function.identity())
                 .collect(Collectors.groupingByConcurrent(
                         Function.identity(),
-                        Collectors.counting()
+                        Collectors.counting() // e conta a frequência de cada número
                 ));
+
         return result;
     }
 
--- src/com/Modulo_4/trabalho/word/WordPractice.java
+++ src/com/Modulo_4/trabalho/word/WordPractice.java
@@ -13,34 +13,38 @@ public class WordPractice {
 
     public List<String> wordsLessThan5Characters() {
         //Filtre apenas palavras que possuem menos de 5 caracteres
-        List<String> wordLessFive = Arrays.stream(WORDS)
-                .filter(word -> word.length() < 5)
-                .collect(Collectors.toList());
+        List<String> wordLessFive = Arrays.stream(WORDS) //cria um stream a partir do array WORDS.
+                .filter(word -> word.length() < 5) //filtra as palavras que possuem menos de 5 caracteres
+                .collect(Collectors.toList()); //coleta as palavras filtradas em uma lista.
 
         return wordLessFive;
     }
     public List<String> words5Characters() {
         // Filtre apenas palavras que possuem 5 caracteres
-        List<String> wordFive = Arrays.stream(WORDS)
-                .filter(words -> words.length() == 5)
-                .collect(Collectors.toList());
+        List<String> wordFive = Arrays.stream(WORDS) //cria um stream a partir do array WORDS.
+                .filter(words -> words.length() == 5) //filtra as palavras que possuem 5 caracteres
+                .collect(Collectors.toList()); //coleta as palavras filtradas em uma lista.
         return wordFive;
 
     }
 
     public List<String> wordsGreaterThan5Characters() {
         // Filtre apenas palavras que possuem mais de 5 caracteres
-        List<String> wordThanFive = Arrays.stream(WORDS)
-                .filter(words -> words.length() > 5)
-                .collect(Collectors.toList());
+        List<String> wordThanFive = Arrays.stream(WORDS) //cria um stream a partir do array WORDS.
+                .filter(words -> words.length() > 5) //filtra as palavras que possuem mais de 5 caracteres.
+                .collect(Collectors.toList()); //coleta as palavras filtradas em uma lista.
+
         return wordThanFive;
     }
 
     public List<String> wordsArePalindromes() {
         // Identifique e retorne as palavras que sejam palíndromos(são iguais de trás para frente, exemplo: Radar)
-        List<String> palindromes = Arrays.stream(WORDS)
+
+        List<String> palindromes = Arrays.stream(WORDS) //cria um stream a partir do array WORDS.
+
+                //filtra as palavras que são iguais ao seu reverso, ignorando a caixa (maiúsculas/minúsculas).
                 .filter(word -> word.equalsIgnoreCase(new StringBuilder(word).reverse().toString()))
-                .collect(Collectors.toList());
+                .collect(Collectors.toList()); //coleta as palavras filtradas em uma lista.
 
         return palindromes;
     }
@@ -48,10 +52,10 @@ public class WordPractice {
     public Map<String, Integer> countLetterEachWord() {
         //EXTRA
         //Calcule quantos caracteres cada palavra têm, o retorno deve ser a palavra -> quantidade de carateres. Exemplo: Osso -> 4
-        Map<String, Integer> caractersWord = Arrays.stream(WORDS)
-                .collect(Collectors.toMap(
-                        Function.identity(),
-                        String::length
+        Map<String, Integer> caractersWord = Arrays.stream(WORDS) //cria um stream a partir do array WORDS.
+                .collect(Collectors.toMap( //coleta as palavras em um mapa, onde:
+                        Function.identity(), //a palavra é usada como chave do mapa.
+                        String::length //a palavra é usada como chave do mapa.
                 ));
 
         return caractersWord;
@@ -61,9 +65,9 @@ public class WordPractice {
     public Integer countLettersAllWordHave() {
         // EXTRA
         // Calcule quantos caracteres tem todas as palavras têm juntas.
-        Integer lettersAllWord = Arrays.stream(WORDS)
-                .mapToInt(String::length)
-                .sum();
+        Integer lettersAllWord = Arrays.stream(WORDS)//cria um stream a partir do array WORDS.
+                .mapToInt(String::length) //converte cada palavra em um inteiro que representa o comprimento da palavra.
+                .sum(); //soma todos os comprimentos das palavras.
 
         return lettersAllWord;
     }
